6dbfe47882bd68dc879c4c45f2951366969b5fb6
Theodore Boudros <theodore.boudros.design@gmail.com>
2017-08-24 13:58:49 +0300

    Building the admin page

--- frontend/required/default/admin/login.php
+++ frontend/required/default/admin/login.php
@@ -7,9 +7,9 @@
     <form action="" method="POST">
         <table>
             <tr><td><center><h2>Hello, Admin!</h2></center></td></tr>
-            <tr><td><input class="center" name="submit" placeholder="Username: " type="text"></input></td></tr>
-            <tr><td><input class="center" name="submit" placeholder="Password: " type="text"></input></td></tr>
-            <tr><td></td></tr>
+            <tr><td><input class="center" name="username" placeholder="Username: " type="text"></input></td></tr>
+            <tr><td><input class="center" name="password" placeholder="Password: " type="password"></input></td></tr>
+            <tr><td><hr></td></tr>
             <tr><td><input class="center" name="submit" value="Log In" type="submit"></input></td></tr>
             <tr><td><a>Did you forget your password?</a></td></tr>
         </table>
