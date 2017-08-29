8b957f5579b8ad0feb61b63b24c69d4190a07e9e
Theodore Boudros <theodore.boudros.design@gmail.com>
2017-08-29 22:03:29 +0300

    Sidebar in the makings!

--- frontend/required/default/admin/sidebar.php
+++ frontend/required/default/admin/sidebar.php
@@ -3,21 +3,12 @@
         <i class="fa fa-user"></i></i> Users 
     </div>
     <div class="sidebarItem">
-        <i class="fa fa-user"></i> Users
+        <i class="fa fa-edit"></i> Posts
     </div>
     <div class="sidebarItem">
-        <i class="fa fa-user"></i> Users
+        <i class="fa fa-magic"></i> Design
     </div>
     <div class="sidebarItem">
-        <i class="fa fa-user"></i> Users
-    </div>
-    <div class="sidebarItem">
-        <i class="fa fa-user"></i> Users
-    </div>
-    <div class="sidebarItem">
-        <i class="fa fa-user"></i> Users
-    </div>
-    <div class="sidebarItem">
-        <i class="fa fa-user"></i> Users
+        <i class="fa fa-cog"></i> Settings
     </div>
 </div>
