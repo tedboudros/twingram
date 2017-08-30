c4d500179b84577bbda8e833e79481fa666dc364
Theodore Boudros <theodore.boudros.design@gmail.com>
2017-08-30 14:35:17 +0300

    Design changes

--- frontend/required/default/admin/posts.php
+++ frontend/required/default/admin/posts.php
@@ -1,8 +1,12 @@
 <table class="admintable" cellspacing=0>
+<thead>
 <tr><th>ID: </th><th>Title: </th><th>Text: </th><th>ID Of the user: </th><th>Creation Date: </th></tr>
+</thead>
+<tbody>
     <?php foreach($db['posts'] as $user){ ?>
 
         <tr><td><?php echo $user['id']; ?></td><td><?php echo $user['title']; ?></td><td><?php echo $user['text']; ?></td><td><?php echo $user['userid']; ?></td><td><?php echo $user['date']; ?></td></tr>
 
     <?php } ?>
+</tbody>
 </table>
--- frontend/required/default/admin/users.php
+++ frontend/required/default/admin/users.php
@@ -1,8 +1,12 @@
 <table class="admintable" cellspacing=0>
+<thead>
 <tr><th>ID: </th><th>Display Name: </th><th>Username: </th><th>Creation Date: </th></tr>
+</thead>
+<tbody>
     <?php foreach($db['users'] as $user){ ?>
 
         <tr><td><?php echo $user['id']; ?></td><td><?php echo $user['displayname']; ?></td><td><?php echo $user['username']; ?></td><td><?php echo $user['date']; ?></td></tr>
 
     <?php } ?>
+</tbody>
 </table>
